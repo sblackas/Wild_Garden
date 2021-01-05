2d01700920bf7bd09bb08b3238ef8cecc761cf80
Vivi <vivianne.traore@hotmail.fr>
2021-01-05 17:13:38 +0100

    sign in + sign up fix

--- backend/routes/routesUsers.js
+++ backend/routes/routesUsers.js
@@ -4,6 +4,7 @@ const db = require('../database/db');
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // le nombre de fois que l'on hashe le mdp
 const jwt = require('jsonwebtoken');
+const config = require('../modules/config');
 
 
 
@@ -16,7 +17,7 @@ router.post('/users/sign-up', function (req, res) {
     console.log(hashpassword);
     console.log(req.body.lastname);
 
-    let newUser = `INSERT INTO users (u_name, u_lastname, u_email, u_password, u_pp) VALUES ('${req.body.name}','${req.body.lastname}','${req.body.lastname}', '${hashpassword}', '${req.body.pp}')`; 
+    let newUser = `INSERT INTO users (u_name, u_lastname, u_email, u_password, u_pp) VALUES ('${req.body.name}','${req.body.lastname}','${req.body.email}', '${hashpassword}', '${req.body.pp}')`; 
     db.query(newUser, function (err, result) { // envoyer mon newUser dans ma database
         if (err) throw err;
         console.log("one user inserted");
@@ -26,6 +27,30 @@ router.post('/users/sign-up', function (req, res) {
 
 });
 
+router.post('/users/sign-in', function(req, res) {
+    // let email = req.body.email
+    // let password = req.body.password
 
+     db.query(`SELECT * FROM users WHERE u_email = '${req.body.email}'`, function (err, result) { // *=tout
+        if (err) throw err;
+        if (result.length) {
+            bcrypt.compare(req.body.password, result[0].u_password, function(err,theuser){
+              console.log(theuser);
+              if(theuser) {
+                let token = jwt.sign({ id: result[0].id_user, name: result[0].u_name }, config.secret, { expiresIn: 86400 });
+                console.log(token);
+                res.send({ auth: true, token: token, user: result[0] }); 
+              } else {
+                res.status(400).send("wrong password") 
+              }
+          })
+  
+          } else {
+            res.status(400).send("sorry we don't know this user") 
+          }
+          
+        });
+  
+    });
 
 module.exports = router;
