3ef5d1b4c6f9d38ad79579e9f492eaa3bd515e6b
Vivi <vivianne.traore@hotmail.fr>
2021-01-18 13:34:47 +0100

    inner join

--- backend/routes/routesFeedback.js
+++ backend/routes/routesFeedback.js
@@ -10,14 +10,14 @@ router.post('/feedback/add', function (req, res) {
        });
    })
 
-// router.get('/get-feedback/:id_user', function (req,res) {
-//    let userId = req.params.id
-//    let getFeedback = `SELECT  users.u_name, users.u_lastname FROM users INNER JOIN feedbacks on users.id_user = feedbacks.id_user`
-//    db.query(getFeedback, function (err, results) {
-//       if (err) throw err,
-//       res.send(results)
-//    })
-// })
+router.get('/get-feedback/:id_user', function (req,res) {
+   let userId = req.params.id
+   let getFeedback = `SELECT  users.u_name, users.u_lastname FROM users INNER JOIN feedbacks on users.id_user = feedbacks.id_user WHERE id_user = '${userID}'`
+   db.query(getFeedback, function (err, results) {
+      if (err) throw err,
+      res.send(results)
+   })
+})
 
 
 
