a02cdbca4550ca0f25f7947c2c1c3b39ea0b1335
Vivi <vivianne.traore@hotmail.fr>
2021-01-18 16:16:51 +0100

    changes

--- backend/routes/routesFeedback.js
+++ backend/routes/routesFeedback.js
@@ -3,15 +3,30 @@ const router = express.Router();
 const db = require('../database/db');
 
 router.post('/feedback/add', function (req, res) {
-    let newFeedback = `INSERT INTO feedbacks (commentary) VALUES ('${req.body.comment}')`; 
-       db.query(newFeedback, function (err, result) { 
-       if (err) throw err;
-        res.send(result);
-       });
-   })
+   let newFeedback = `INSERT INTO feedbacks (commentary) VALUES ('${req.body.comment}')`;
+   db.query(newFeedback, function (err, result) {
+      if (err) throw err;
+      res.send(result);
+   });
+})
+
+router.delete('/feedback/:id_feedback', function (req, res) {
+   console.log(req.body);
+   db.query(`DELETE FROM feedbacks WHERE id_feedback = '${req.params.id_feedback}'`, function (error, results) {
+      if (error) throw error;
+      res.send('Commentary has been deleted!');
+   });
+});
+
+router.put('/feedback/:edit', function (req, res) {
+   db.query(`UPDATE feedbacks SET commentary = '${req.body.comment}' WHERE id_feedback = '${req.params.edit}'`, function (error, results) {
+      if (error) throw error;
+      res.json('THE COMMENT HAS BEEN MODIFIED');
+   });
+});
 
 //_____Recuperer tous les commentaires posté par un user
-router.get('/get-feedback-user/:id', function (req,res) {
+router.get('/get-feedback-user/:id', function (req, res) {
    let userId = req.params.id
    let getFeedback = `SELECT  users.u_name, users.u_lastname, feedbacks.commentary FROM users INNER JOIN feedbacks on users.id_user = feedbacks.id_user WHERE id_user = '${userId}'`
    db.query(getFeedback, function (err, results) {
@@ -21,7 +36,7 @@ router.get('/get-feedback-user/:id', function (req,res) {
 })
 
 //_____Recuperer tous les commentaires postés sur une oeuvre
-router.get('/get-feedback-artwork/:id', function (req,res) {
+router.get('/get-feedback-artwork/:id', function (req, res) {
    let artFeedbackId = req.params.id
    let getFeedbackArt = `SELECT  feedbacks.commentary, artworks.art_title, artworks.art_picture FROM feedbacks INNER JOIN artworks on feedbacks.id_artwork = artworks.id_artwork WHERE id_artwork = '${artFeedbackId}'`
    db.query(getFeedbackArt, function (err, results) {
@@ -32,4 +47,4 @@ router.get('/get-feedback-artwork/:id', function (req,res) {
 
 
 
-   module.exports = router;
+module.exports = router;
--- backend/routes/routesUsers.js
+++ backend/routes/routesUsers.js
@@ -9,98 +9,98 @@ const config = require('../modules/config');
 
 
 router.post('/users/sign-up', function (req, res) {
-    const password = req.body.password;
-    let hashpassword = bcrypt.hashSync(password, saltRounds)
-    console.log(req.body.password);
-    console.log(req.body.email);
-    console.log(req.body.name);
-    console.log(hashpassword);
-    console.log(req.body.lastname);
+  const password = req.body.password;
+  let hashpassword = bcrypt.hashSync(password, saltRounds)
+  console.log(req.body.password);
+  console.log(req.body.email);
+  console.log(req.body.name);
+  console.log(hashpassword);
+  console.log(req.body.lastname);
 
-    let newUser = `INSERT INTO users (u_name, u_lastname, u_email, u_password, u_pp) VALUES ('${req.body.name}','${req.body.lastname}','${req.body.email}', '${hashpassword}', '${req.body.pp}')`; 
-    db.query(newUser, function (err, result) { // envoyer mon newUser dans ma database
-        if (err) throw err;
-        console.log("one user inserted");
-        res.send(result)
+  let newUser = `INSERT INTO users (u_name, u_lastname, u_email, u_password, u_pp) VALUES ('${req.body.name}','${req.body.lastname}','${req.body.email}', '${hashpassword}', '${req.body.pp}')`;
+  db.query(newUser, function (err, result) { // envoyer mon newUser dans ma database
+    if (err) throw err;
+    console.log("one user inserted");
+    res.send(result)
 
-    });
+  });
 
 });
 
-router.post('/users/sign-in', function(req, res) {
-    // let email = req.body.email
-    // let password = req.body.password
+router.post('/users/sign-in', function (req, res) {
+  // let email = req.body.email
+  // let password = req.body.password
 
-     db.query(`SELECT * FROM users WHERE u_email = '${req.body.email}'`, function (err, result) { // *=tout
-        if (err) throw err;
-        if (result.length) {
-            bcrypt.compare(req.body.password, result[0].u_password, function(err,theuser){
-              console.log(theuser);
-              if(theuser) {
-                let token = jwt.sign({ id: result[0].id_user, name: result[0].u_name }, config.secret, { expiresIn: 86400 });
-                console.log(token);
-                res.send({ auth: true, token: token, user: result[0] }); 
-              } else {
-                res.status(400).send("wrong password") 
-              }
-          })
-  
-          } else {
-            res.status(400).send("sorry we don't know this user") 
-          }
-          
-        });
-  
-    });
-
-
-    router.get('/users', function (req,res) {
-        let allUsers = `SELECT id_user,u_name FROM users`;
-        db.query(allUsers, function (err, todoUser) {
-     
-          if (err) res.send (err);
-              console.log(todoUser);
-              res.send(todoUser)
-        })
-        
+  db.query(`SELECT * FROM users WHERE u_email = '${req.body.email}'`, function (err, result) { // *=tout
+    if (err) throw err;
+    if (result.length) {
+      bcrypt.compare(req.body.password, result[0].u_password, function (err, theuser) {
+        console.log(theuser);
+        if (theuser) {
+          let token = jwt.sign({ id: result[0].id_user, name: result[0].u_name }, config.secret, { expiresIn: 86400 });
+          console.log(token);
+          res.send({ auth: true, token: token, user: result[0] });
+        } else {
+          res.status(400).send("wrong password")
+        }
       })
 
+    } else {
+      res.status(400).send("sorry we don't know this user")
+    }
 
-      router.get('/users/:id_user', function (req, res) {
-        try {
-            db.query(`SELECT u_name, u_lastname, u_email, u_pp FROM users WHERE id_user = '${req.params.id_user}'`, (err, result) => {
-                if (err) throw err
-                console.log(result);
-                res.json(result)
+  });
 
-            })
-        } catch (error) {
-            console.log(error);
-        }
+});
+
+
+router.get('/users', function (req, res) {
+  let allUsers = `SELECT id_user,u_name FROM users`;
+  db.query(allUsers, function (err, todoUser) {
+
+    if (err) res.send(err);
+    console.log(todoUser);
+    res.send(todoUser)
+  })
+
+})
+
+
+router.get('/users/:id_user', function (req, res) {
+  try {
+    db.query(`SELECT u_name, u_lastname, u_email, u_pp FROM users WHERE id_user = '${req.params.id_user}'`, (err, result) => {
+      if (err) throw err
+      console.log(result);
+      res.json(result)
+
     })
-    
-    router.delete('/users/:id_user', function (req, res) {
-      console.log(req.body);
-      db.query(`DELETE FROM users WHERE id_user = '${req.params.id_user}'`, function (error, results) {
-         if (error) throw error;
-         res.send('User has been deleted!');
-       });
-   });
+  } catch (error) {
+    console.log(error);
+  }
+})
 
-   router.put('/users/:edit', function (req, res) {
-      db.query(`UPDATE users SET u_name = '${req.body.name}', u_lastname = '${req.body.lastname}', u_email = '${req.body.email}', u_password = '${req.body.password}', u_pp = '${req.body.pp}' WHERE id_user = '${req.params.edit}'` , function (error, results) {
+router.delete('/users/:id_user', function (req, res) {
+  console.log(req.body);
+  db.query(`DELETE FROM users WHERE id_user = '${req.params.id_user}'`, function (error, results) {
     if (error) throw error;
-     res.send(JSON.stringify(results + "PROFILE HAS BEEN UPDATED"));
-    //  res.status(200).send("PROFILE HAS BEEN UPDATED");
-   });
- });
+    res.send('User has been deleted!');
+  });
+});
 
- router.get('/get-artworks/:id', function (req,res) {
+router.put('/users/:edit', function (req, res) {
+  db.query(`UPDATE users SET u_name = '${req.body.name}', u_lastname = '${req.body.lastname}', u_email = '${req.body.email}', u_password = '${req.body.password}', u_pp = '${req.body.pp}' WHERE id_user = '${req.params.edit}'`, function (error, results) {
+    if (error) throw error;
+    res.send(JSON.stringify(results + "PROFILE HAS BEEN UPDATED"));
+    //  res.status(200).send("PROFILE HAS BEEN UPDATED");
+  });
+});
+
+router.get('/get-artworks/:id', function (req, res) {
   let userId = req.params.id
   let getArtworks = `SELECT  users.u_name, users.u_lastname, artworks.art_title, artworks.art_desc, artworks.picture FROM users INNER JOIN artworks on users.id_user = artworks.id_user WHERE id_user = '${userId}'`
   db.query(getArtworks, function (err, results) {
-     if (err) throw err,
-     res.send(results)
+    if (err) throw err,
+      res.send(results)
   })
 })
 
