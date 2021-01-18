c7d4746f189f9a5db2bee45e8548c5a7b888f0df
Vivi <vivianne.traore@hotmail.fr>
2021-01-18 15:31:13 +0100

    get all art from user

--- backend/routes/routesArtworks.js
+++ backend/routes/routesArtworks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db');
 
-
+//______Ajouter une oeuvre
 router.post('/artwork/add', function (req, res) {
  let newArtwork = `INSERT INTO artworks (art_title, art_desc, art_picture) VALUES ('${req.body.name}','${req.body.description}','${req.body.picture}')`; 
     db.query(newArtwork, function (err, result) { 
@@ -11,6 +11,7 @@ router.post('/artwork/add', function (req, res) {
     });
 })
 
+//______Recuperer toutes les infos concernant une oeuvre
 router.get('/artwork/:id_artwork', function (req, res) {
     try {
         db.query(`SELECT art_title, art_desc, art_picture FROM artworks WHERE id_artwork = '${req.params.id_artwork}'`, (err, result) => {
@@ -24,6 +25,7 @@ router.get('/artwork/:id_artwork', function (req, res) {
     }
 })
 
+//_____Supprimer une oeuvre
 router.delete('/artwork/:id_artwork', function (req, res) {
     console.log(req.body);
     db.query(`DELETE FROM artworks WHERE id_artwork = '${req.params.id_artwork}'`, function (error, results) {
@@ -32,11 +34,22 @@ router.delete('/artwork/:id_artwork', function (req, res) {
          });
    });
 
+//______Pouvoir modifier son oeuvre   
 router.put('/artwork/:edit', function (req, res) {
     db.query(`UPDATE artworks SET art_title = '${req.body.name}', art_desc = '${req.body.description}', art_picture = '${req.body.picture}' WHERE id_artwork = '${req.params.edit}'` , function (error, results) {
   if (error) throw error;
    res.json('THE ARTWORK HAS BEEN UPDATED');
      });
   });
+
+//_______Recuperer toutes les oeuvres poster par un user
+  router.get('/get-artworks/:id', function (req,res) {
+    let userId = req.params.id
+    let getArtworks = `SELECT  users.u_name, users.u_lastname, artworks.art_title, artworks.art_desc, artworks.picture FROM users INNER JOIN artworks on users.id_user = artworks.id_user WHERE id_user = '${userId}'`
+    db.query(getArtworks, function (err, results) {
+       if (err) throw err,
+       res.send(results)
+    })
+ })
 
 module.exports = router;
