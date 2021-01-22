845c8816771629b093975d6e797aceadf353c454
Vivi <vivianne.traore@hotmail.fr>
2021-01-22 15:01:58 +0100

    changes

--- frontend/chefdoeuvre/src/ComponentsHome/SignIn.js
+++ frontend/chefdoeuvre/src/ComponentsHome/SignIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import {Button, Alert, Form} from 'react-bootstrap'
-import {Redirect} from 'react-router-dom'
-import {Alert} from 'react-bootstrap'
+// import {Button,Form} from 'react-bootstrap'
+// import {Redirect} from 'react-router-dom'
+// import {Alert} from 'react-bootstrap'
 import axios from 'axios'
 import Header from './Header'
 import jwt from 'jsonwebtoken';
@@ -13,22 +13,23 @@ class SignIn extends React.Component{
     state = {
         email: "",
         password: "",
-        message: "",
+        message: null,
         redirection: false
     
     };
     
     // Chaque input necessite sa fonction
-    inputEmail = event => {
+    inputEmailSignIn = event => {
         this.setState({email:event.target.value})
     };
-    inputPassword = event => {
+    inputPasswordSignIn = event => {
         this.setState({password:event.target.value})
     };
     
     // fonction pour notre Submit
     handleSubmit = event => {
         event.preventDefault();
+        console.log('handleSubmit');
     
         const user = {
             email: this.state.email,
@@ -38,8 +39,6 @@ class SignIn extends React.Component{
         axios.post('http://localhost:8000/users/sign-in', user)
         .then(res => {
             this.setState({ redirection: true})
-            console.log(res);                   // -----> ces lignes étaient là avant qu'on fasse le store
-            console.log(res.data);
             if(res.status === 200) {
               console.log(res);
               let decoded = jwt.decode(res.data.token);
@@ -49,22 +48,33 @@ class SignIn extends React.Component{
                 id: decoded.id
               };
               
-              this.props.loginUser(loggedUser)
-            //   this.props.history.push('/dashboard');
-            } 
-            localStorage.setItem("token", res.data.token) //Une fois que ca donne un token il faut le stocker, je le recupere dans addProduct
-        })
-        .catch(err => {
-          console.log(err.res.data);
-          this.setState({ message: err.res.data })
-          console.log(this.state.message);
+              // this.props.loginUser(loggedUser)
+              console.log(loggedUser);
+              this.props.history.push('/dashboard');
+              localStorage.setItem("token", res.data.token) //Une fois que ca donne un token il faut le stocker, je le recupere dans addProduct
+
+            } else if (res.status === 203) {
+              console.log("else if 203");
+              this.setState({message: res.data})
+              console.log(res.data);
+            }
+        
+          })
+        .catch(error => {
+          console.log("catch error");
+          this.setState({redirection: false})
+          console.log(error);
         })
     }
+
+
+
+
 render() {
-    const { redirection } = this.state;
-    if (redirection){
-        return <Redirect to='/dashboard/'/>
-    }
+    // const { redirection } = this.state;
+    // if (redirection){
+    //     return <Redirect to='/dashboard/'/>
+    // } 
 
   return (
       <div>
@@ -78,17 +88,18 @@ render() {
 <p>To keep enjoy our content please login with your personal info</p>
 <br></br>
   <form onSubmit={this.handleSubmit}>
-  { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null }
+  {/* { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null } */}
+  { this.state.message && <p> {this.state.message} </p> }
     <div className="user-box">
-      <input type="email" onChange={this.inputEmail}/>
+      <input type="email" onChange={this.inputEmailSignIn}/>
       <label>Email</label>
     </div>
     <div className="user-box">
-      <input type="password"  onChange={this.inputPassword} />
+      <input type="password"  onChange={this.inputPasswordSignIn} />
       <label>Password</label>
     </div>
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
-    <button id="btn" type="submit"><span className="noselect">Submit</span><div id="circle"></div></button>
+    <button id="btn1" type="submit"><span className="noselect">Submit</span><div id="circle"></div></button>
 
   </form>
 </div>
@@ -99,17 +110,17 @@ render() {
 <p>To keep enjoy our content please login with your personal info</p>
 <br></br>
   <form onSubmit={this.handleSubmit}>
-  { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null }
+  {/* { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null } */}
     <div className="user-box">
-      <input type="email" onChange={this.inputEmail}/>
+      <input type="email" onChange={this.inputEmailSignIn}/>
       <label>Email</label>
     </div>
     <div className="user-box">
-      <input type="password"  onChange={this.inputPassword} />
+      <input type="password"  onChange={this.inputPasswordSignIn} />
       <label>Password</label>
     </div>
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
-    <button id="btn" type="submit"><span className="noselect">Submit</span><div id="circle"></div></button>
+    <button id="btn2" type="submit"><span className="noselect">Submit</span><div id="circle"></div></button>
 
   </form>
 </div>
@@ -120,13 +131,13 @@ render() {
 <p>To keep enjoy our content please login with your personal info</p>
 <br></br>
   <form onSubmit={this.handleSubmit}>
-  { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null }
+  {/* { this.state.message ?  <Alert variant="danger" > {this.state.message} </Alert> : null } */}
     <div className="user-box">
-      <input type="email" onChange={this.inputEmail}/>
+      <input type="email" onChange={this.inputEmailSignIn}/>
       <label>Email</label>
     </div>
     <div className="user-box">
-      <input type="password"  onChange={this.inputPassword} />
+      <input type="password"  onChange={this.inputPasswordSignIn} />
       <label>Password</label>
     </div>
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
--- frontend/chefdoeuvre/src/ComponentsUser/ArtworksList.js
+++ frontend/chefdoeuvre/src/ComponentsUser/ArtworksList.js
@@ -1,11 +1,32 @@
-// import React from 'react';
+import React from 'react';
 // import HeaderUser from '../ComponentsUser/HeaderUser'
 // import axios from 'axios'
 // import { Link } from 'react-router-dom';
 // import Button from 'react-bootstrap/esm/Button';
 // import Card from 'react-bootstrap/Card'
 // import ListGroup from 'react-bootstrap/ListGroup'
-// import ListGroupItem from 'react-bootstrap/ListGroupItem'
+// import ListGroupItem from 'react-bootstrap/ListG
+
+// import React, { Component } from 'react'
+
+class ArtworksList extends React.Component {
+
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ArtworksList
+
+
+
 
 
 
